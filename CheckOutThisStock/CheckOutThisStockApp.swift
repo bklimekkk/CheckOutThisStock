c9d4b83db259ae7d1c0b0073bd2a1d872be0bdd7
bklimekkk <klimekbartosz12@gmail.com>
2022-01-24 00:39:19 +0100

    Initial Commit

---
+++ CheckOutThisStock/CheckOutThisStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckOutThisStockApp.swift
+//  CheckOutThisStock
+//
+//  Created by Bartosz Klimek on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CheckOutThisStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
